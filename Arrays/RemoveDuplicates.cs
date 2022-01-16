e377d6d8eb6855c473ebe115fe55a14255cc5299
Bhawna Gupta <programmingwithgupta@gmail.com>
2022-01-16 19:06:39 +0530

    Remove Duplicates

---
+++ Arrays/RemoveDuplicates.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        // 0 1 1 1 2 2 4 5 6 
+        if (nums.Length == 0)
+            return 0;
+        int j = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[j])
+            {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
